757bebad1f9e3f2d77618b3ad9e3ccf4a7aedd34
Maksim Yakimenko <ghastlive@gmail.com>
2021-09-20 11:50:03 +0300

    Добавлен комментарий

--- src/test/java/CucumberRunnerTest.java
+++ src/test/java/CucumberRunnerTest.java
@@ -3,6 +3,7 @@ import io.cucumber.junit.CucumberOptions;
 
 import org.junit.runner.RunWith;
 
+//Это кукумбер
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = {"src/test/resources"},
